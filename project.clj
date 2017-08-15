003e1fa3cf6b8441849c065a2e596d6f3e06046c
Steve Chan <chanshunli@gmail.com>
2017-08-15 15:02:25 +0800

    sparkling-ml-example.core=> (-main) ("Testing spark" "and sparkling")

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [gorillalabs/sparkling "2.1.2"]]
+  :aot [#".*" sparkling.serialization sparkling.destructuring]
+  :main sparkling-ml-example.core
+  :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.11 "2.2.0"]
+                                       [org.apache.spark/spark-sql_2.11 "2.2.0"]]}
+             :dev {:plugins [[lein-dotenv "RELEASE"]]}})
--- src/sparkling_ml_example/core.clj
+++ src/sparkling_ml_example/core.clj
@@ -1,6 +1,21 @@
-(ns sparkling-ml-example.core)
+(ns sparkling-ml-example.core
+  (:require [sparkling.conf :as conf]
+            [sparkling.core :as spark]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  (spark/with-context
+    sc (-> (conf/spark-conf)
+           (conf/app-name "sparkling-test")
+           (conf/master "local"))
+    (let [lines-rdd
+          (spark/into-rdd
+           sc ["This is a firest line"
+               "Testing spark"
+               "and sparkling"
+               "Happy hacking!"])]
+      (spark/collect                      
+       (spark/filter                     
+        #(.contains % "spark")          
+        lines-rdd)))))
+
