f63c59ea73902e5327d7fa4be9f271a83a5929c6
Steve Chan <chanshunli@gmail.com>
2017-08-15 14:42:44 +0800

    lein new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sparkling-ml-example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sparkling_ml_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns sparkling-ml-example.core-test
+  (:require [clojure.test :refer :all]
+            [sparkling-ml-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
