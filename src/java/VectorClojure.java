7e6b6ad7335d9875a2f72729c08e22ce37e64091
Steve Chan <chanshunli@gmail.com>
2017-08-18 16:11:19 +0800

    Spark的版本问题导致失败了。。。

--- src/java/VectorClojure.java
+++ src/java/VectorClojure.java
@@ -1,8 +1,10 @@
 package sparkinterface;
-import org.apache.spark.ml.linalg.Vector;
-import org.apache.spark.ml.linalg.Vectors;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.regression.StreamingLinearRegressionWithSGD;
 
 public class VectorClojure {
+    
     public static Vector dense(String[] args) {
         double[] prompt = new double[args.length];
         for (int i =0; i<args.length; i++){
@@ -11,4 +13,20 @@ public class VectorClojure {
         Vector denseVec = Vectors.dense(prompt);
         return denseVec;
     }
+    
+    public static StreamingLinearRegressionWithSGD model(int num, int iter) {
+//        StreamingLinearRegressionWithSGD slr = new StreamingLinearRegressionWithSGD()
+//            .setNumIterations(2)
+//            .setInitialWeights(Vectors.dense(0.0));
+        StreamingLinearRegressionWithSGD model = new StreamingLinearRegressionWithSGD()
+            .setStepSize(0.5)
+            .setNumIterations(10)
+            .setInitialWeights(Vectors.dense(0.0));
+//        StreamingLinearRegressionWithSGD slr = new StreamingLinearRegressionWithSGD()
+//            .setNumIterations(iter)
+//            .setInitialWeights(Vectors.dense(0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0))
+//            .setStepSize(0.01);
+        return model;
+    }
+    
 }
